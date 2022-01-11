ea5eec3e528be4c7576b9b340412a10721ed3640
Saleh Ahmed Panna <panna.ahmed09@gmail.com>
2022-01-11 12:11:33 +0600

    Auth with seller and bidder validation

--- src/handlers/placeBid.js
+++ src/handlers/placeBid.js
@@ -11,8 +11,16 @@ async function placeBid(event, context) {
   let updatedAuction;
   const { id } = event.pathParameters;
   const { amount } = event.body;
+  const { email } = event.requestContext.authorizer;
 
   const auction = await getAuctionById(id);
+  if (email === auction.seller) {
+    throw new createError.Forbidden('You cannot bid on your own auction.');
+  }
+
+  if (email === auction.highestBid.bidder) {
+    throw new createError.Forbidden('You are already the highest bidder');
+  }
 
   if (auction.status !== 'OPEN') {
     throw new createError.Forbidden('You cannot bid on closed auction.');
@@ -27,9 +35,11 @@ async function placeBid(event, context) {
   const params = {
     TableName: process.env.AUCTIONS_TABLE_NAME,
     Key: { id },
-    UpdateExpression: 'set highestBid.amount = :amount',
+    UpdateExpression:
+      'set highestBid.amount = :amount, highestBid.bidder = :bidder',
     ExpressionAttributesValues: {
       ':amount': amount,
+      ':bidder': email,
     },
     ReturnValues: ' ALL_NEW',
   };
